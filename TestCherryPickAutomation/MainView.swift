367fcd9679b86aafc838c5dad2396f05053b1eea
David Para <davidjpara@icloud.com>
2021-06-17 13:27:31 -0500

    Converted from UIKit to SwiftUI

---
+++ TestCherryPickAutomation/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  TestCherryPickAutomation
+//
+//  Created by David Para on 6/17/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ TestCherryPickAutomation/TestCherryPickAutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCherryPickAutomationApp.swift
+//  TestCherryPickAutomation
+//
+//  Created by David Para on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TestCherryPickAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
