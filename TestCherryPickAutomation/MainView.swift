abf63ade9a7e8c9e57e34cdc46ed1cb3635de864
David Para <davidjpara@icloud.com>
2021-06-22 17:59:30 -0500

    Hello world

--- TestCherryPickAutomation/MainView.swift
+++ TestCherryPickAutomation/MainView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Hello, World!")
         }
     }
 }
